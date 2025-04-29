fed5a538c5118493a14dba51cd0425a6a61fa262
Justin <fan.justin4.1@gmail.com>
2025-04-28 17:38:51 -0700

    accounts to table

---
+++ frontend/lib/supabaseClient.js
@@ -0,0 +1,9 @@
+// lib/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+// Your environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Initialize Supabase client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
