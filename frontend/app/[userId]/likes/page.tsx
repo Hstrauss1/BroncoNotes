3511137f0a15170480626961887a8aff7430629e
501A <art71903design@gmail.com>
2025-06-02 18:07:12 -0700

    Refactor LikesPage to use ListLikeNotes component for displaying liked notes

--- frontend/app/[userId]/likes/page.tsx
+++ frontend/app/[userId]/likes/page.tsx
@@ -1,10 +1,7 @@
-import { Note } from "@/app/types";
+import { NoteLinkSkeleton } from "@/components/ui/note-link";
 import { createClient } from "@/lib/supabase/server";
-
-type LikedNote = {
-  user_id: string;
-  notes: Note[];
-};
+import { Suspense } from "react";
+import ListLikeNotes from "./ListLikedNotes";
 
 export default async function LikesPage({
   params,
@@ -15,47 +12,23 @@ export default async function LikesPage({
   const supabase = await createClient();
   const session = await supabase.auth.getSession();
   const token = session.data.session?.access_token;
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/liked-notes/${userId}/notes`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-
-  if (!res.ok) {
-    return (
-      <section className="w-full h-full">
-        <h1>Error fetching liked notes</h1>
-      </section>
-    );
-  }
-
-  const data = (await res.json()) as LikedNote;
-  const noteIds = data.notes;
-  console.log(data);
+  if (!token) return null;
   return (
     <section className="w-full h-full">
-      <hgroup className="wm-auto">
-        <h1 className="pt-10 pb-5">Liked Notes</h1>
-        <br />
-        {noteIds.length > 0 ? (
-          <div className="w-full grid gap-4">
-            {noteIds.map((note) => (
-              <div key={note.note_id} className="p-4 border rounded shadow">
-                <h2 className="font-bold">{note.title}</h2>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="w-full text-center text-gray-500">
-            No liked notes.
-          </div>
-        )}
-      </hgroup>
+      <div className="wm-auto flex flex-col gap-6 pt-10">
+        <h1>Liked Notes</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <Suspense
+            fallback={Array(4)
+              .fill(0)
+              .map((_, index) => (
+                <NoteLinkSkeleton key={index} />
+              ))}
+          >
+            <ListLikeNotes userId={userId} token={token} />
+          </Suspense>
+        </div>
+      </div>
     </section>
   );
 }
